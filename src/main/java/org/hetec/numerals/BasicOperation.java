c7474190e94446187ac264234a98cae582aa3929
hetec <hebner.tec@gmail.com>
2015-11-04 15:11:23 +0100

    Create enum for basic binary operations

---
+++ src/main/java/org/hetec/numerals/BasicOperation.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.hetec.numerals;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.hetc.binaryNumber.BinaryNumber;
+
+/**
+ *
+ * @author patrick
+ */
+public enum BasicOperation {
+    ADD("+"){
+        @Override
+        BinaryNumber apply(BinaryNumber bin1, BinaryNumber bin2){
+            return bin1.add(bin2);
+        }
+    },
+    SUBSTRACT("-"){
+        @Override
+        BinaryNumber apply(BinaryNumber bin1, BinaryNumber bin2){
+            return bin1.subtract(bin2);
+        }
+    },
+    MULTIPLY("*"){
+        @Override
+        BinaryNumber apply(BinaryNumber bin1, BinaryNumber bin2){
+            return bin1.multiply(bin2);
+        }
+    },
+    DIVIDE("/"){
+        @Override
+        BinaryNumber apply(BinaryNumber bin1, BinaryNumber bin2){
+            return bin1.divide(bin2);
+        }
+    };
+    
+    private final String sign;
+    
+    private static final Map<String, BasicOperation> signsToOps = new HashMap<>();
+    
+    BasicOperation(String sign){
+        this.sign = sign;
+    }
+    
+    static {
+        for(BasicOperation op : BasicOperation.values()){
+            signsToOps.put(op.sign, op);
+        }
+    }
+    
+    abstract BinaryNumber apply(BinaryNumber bin1, BinaryNumber bin2);
+    
+    static BasicOperation fromString(String sign){
+        return BasicOperation.signsToOps.get(sign);
+    }
+}
