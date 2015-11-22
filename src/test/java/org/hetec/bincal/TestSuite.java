1ae90189c7cec3930bd55c87c0428d8d6bf40fdf
hetec <hebner.tec@gmail.com>
2015-11-22 21:38:23 +0100

    Add test suite

---
+++ src/test/java/org/hetec/bincal/TestSuite.java
@@ -0,0 +1,17 @@
+package org.hetec.bincal;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by patrick on 22.11.15.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        ConversionServletTest.class
+})
+public class TestSuite {
+
+}
